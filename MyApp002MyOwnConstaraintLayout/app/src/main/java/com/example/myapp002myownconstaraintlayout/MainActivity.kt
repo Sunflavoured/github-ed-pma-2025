6be2d04fd83e8716e10097f201170c0b8f637bef
Erika Dmytrenkova <erika.dmytrenkova@bizztreat.com>
2025-10-02 15:19:38 +0200

    #2 uprava ukolu constraint layout

--- MyApp002MyOwnConstaraintLayout/app/src/main/java/com/example/myapp002myownconstaraintlayout/MainActivity.kt
+++ MyApp002MyOwnConstaraintLayout/app/src/main/java/com/example/myapp002myownconstaraintlayout/MainActivity.kt
@@ -1,20 +1,45 @@
 package com.example.myapp002myownconstaraintlayout
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val etName = findViewById<EditText>(R.id.etName)
+        val etSurname = findViewById<EditText>(R.id.etSurname)
+        val etPlace = findViewById<EditText>(R.id.etPlace)
+        val etAge = findViewById<EditText>(R.id.etAge)
+        val etPhone = findViewById<EditText>(R.id.etPhone)
+        val tvInformation = findViewById<TextView>(R.id.tvInformation)
+        val btnSend = findViewById<Button>(R.id.btnSend)
+        val btnDelete = findViewById<Button>(R.id.btnDelete)
+
+        // tlačítko Uložit
+        btnSend.setOnClickListener {
+            val name = etName.text.toString()
+            val surname = etSurname.text.toString()
+            val place = etPlace.text.toString()
+            val age = etAge.text.toString()
+            val phone = etPhone.text.toString()
+
+            val formattedText = "Jmenuji se $name $surname. Je mi $age let, bydlím v $place a můj telefon je $phone."
+            tvInformation.text = formattedText
+        }
+
+        // tlačítko Vymazat
+        btnDelete.setOnClickListener {
+            etName.text.clear()
+            etSurname.text.clear()
+            etPlace.text.clear()
+            etAge.text.clear()
+            etPhone.text.clear()
+            tvInformation.text = ""
         }
     }
 }
