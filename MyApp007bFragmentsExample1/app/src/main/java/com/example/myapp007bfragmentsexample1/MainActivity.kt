c84a4002ea56243694b5e6cc92aad3caa43533ba
Erika Dmytrenkova <erika.dmytrenkova@bizztreat.com>
2025-10-16 11:27:26 +0200

    # 6 zprovoznění zobrazení fragmentů v mainActivity

--- MyApp007bFragmentsExample1/app/src/main/java/com/example/myapp007bfragmentsexample1/MainActivity.kt
+++ MyApp007bFragmentsExample1/app/src/main/java/com/example/myapp007bfragmentsexample1/MainActivity.kt
@@ -5,16 +5,39 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.myapp007bfragmentsexample1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnFragment.setOnClickListener {
+            replaceFragment(Fragment1())
         }
+
+        binding.btnFragment2.setOnClickListener {
+            replaceFragment(Fragment2())
+        }
+        }
+
+    private fun replaceFragment(fragment: Fragment) {
+        // získání instance správce Fragmentu
+        val fragmentManager = supportFragmentManager
+        // vytvoření transakce
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        //nahrazení fragmentu ve view conteineru fragmentem novým, který byl předán jako argument
+
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+
+        //potvrzení transakce a  provedení výměny fragmentu
+        fragmentTransaction.commit()
+
+
     }
-}
+    }
