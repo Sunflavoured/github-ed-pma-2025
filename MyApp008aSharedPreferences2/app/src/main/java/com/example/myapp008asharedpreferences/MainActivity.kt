78b4e9893d194e69e2b4ae362b067079f5024894
Erika Dmytrenkova <erika.dmytrenkova@bizztreat.com>
2025-10-16 15:13:52 +0200

    #7 Zprovoznění layoutu, bez ukladani

--- MyApp008aSharedPreferences2/app/src/main/java/com/example/myapp008asharedpreferences/MainActivity.kt
+++ MyApp008aSharedPreferences2/app/src/main/java/com/example/myapp008asharedpreferences/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myapp008asharedpreferences
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -20,5 +21,30 @@ class MainActivity : AppCompatActivity() {
         val sharedPreferences = getSharedPreferences("sharedPrefs", MODE_PRIVATE)
         val editor = sharedPreferences.edit()
 
+        binding.btnUlozit.setOnClickListener {
+            val jmeno = binding.etJmeno.text.toString()
+            val ageString = binding.etVek.text.toString().trim()
+
+            if (ageString.isBlank()) {
+                Toast.makeText(this, "Zadejte věk", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                val age = ageString.toInt()
+                val isAdult = binding.checkBoxDone.isChecked
+                if ((age < 18 && isAdult) || (age >= 18 && !isAdult)) {
+                    Toast.makeText(this, "Co zkoušíš, mladej?", Toast.LENGTH_SHORT).show()
+                }
+                else {
+                    Toast.makeText(this, "Ukládám data...", Toast.LENGTH_SHORT).show()
+                    editor.apply {
+                        putString("JMENO", jmeno)
+                        putInt("VEK", age)
+                        putBoolean("ISADULT", isAdult)
+                        apply()
+                    }
+                }
+            }
         }
     }
+}
+
