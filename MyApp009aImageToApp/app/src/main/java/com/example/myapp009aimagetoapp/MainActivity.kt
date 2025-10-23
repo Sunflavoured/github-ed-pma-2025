7e13906a678e8b65e51ea9a0dc0f28051da56224
Erika Dmytrenkova <erika.dmytrenkova@bizztreat.com>
2025-10-23 11:24:17 +0200

    nacitani obrazku z galerie

--- MyApp009aImageToApp/app/src/main/java/com/example/myapp009aimagetoapp/MainActivity.kt
+++ MyApp009aImageToApp/app/src/main/java/com/example/myapp009aimagetoapp/MainActivity.kt
@@ -2,19 +2,33 @@ package com.example.myapp009aimagetoapp
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapp009aimagetoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //registerForActivityResult - Až uživatel vybere soubor (např. obrázek), vrať mi výsledek jako uri a já s ním něco udělám
+        //
+        val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+           //spustí se po vybrání obrázku a uloží obrázek do proměnný
+            binding.ivImage.setImageURI(uri)
+        }
+
+        binding.btnTakeImage.setOnClickListener {
+            //tento příkaz zpustí výběr obrázku z telefonu
+            // Vybraný obrázek se zobrazí v ivImage
+            getContent.launch("image/*")
         }
     }
 }
+
