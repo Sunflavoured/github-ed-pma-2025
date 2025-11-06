6a12fb8e61b8eb604b1297a12fc916c3aefd5082
Erika Dmytrenkova <erika.dmytrenkova@bizztreat.com>
2025-11-06 11:37:54 +0100

    #17 přidání abstract class database NoteHubDatabase

---
+++ MyApp012aMyNoteHub/app/src/main/java/com/example/myapp012amynotehub/data/NoteHubDatabase.kt
@@ -0,0 +1,13 @@
+package com.example.myapp012amynotehub.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [Note::class],
+    version = 1,
+    exportSchema = false)
+
+abstract class NoteHubDatabase: RoomDatabase() {
+    abstract fun noteDao(): NoteDao
+}
